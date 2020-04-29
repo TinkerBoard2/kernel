bd4877e49393c9551bd462901d18299f0ba614e2
yupei_lin <yupei_lin@asus.com>
2020-04-29 19:40:22 +0800

    Ethernet : change LED configuration form "Mode A" to "Mode B".
    Change-Id: Ic31931f572493f77a2a7a745266896941c0905a8

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -693,7 +693,7 @@ void set_led_configuration(struct phy_device *phydev)
 	printk("%s: #### before setting led, Reg16 = 0x%x\n", __func__, phy_read(phydev, 16));
 
 	//LED Link speed default setting
-	phy_write(phydev, 16, 0x091b);
+	phy_write(phydev, 16, 0x8910);
 	printk("%s: #### after setting led, Reg16 = 0x%x\n", __func__, phy_read(phydev, 16));
 
 	//switch to PHY`s Page0
