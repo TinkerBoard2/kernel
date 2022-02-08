10106f829fc50671b988e0abdba4171efcba35a4
singhome1_lee <singhome1_lee@asus.com>
2022-02-08 21:13:52 +0800

    ft5406: start to poll touch event.
    Change-Id: Ie8f63700421c148c2f507a8f2b2e439bc02bce4d

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -173,7 +173,7 @@ extern int tinker_mcu_ili9881c_set_bright(int bright, int dsi_id);
 extern int tinker_mcu_ili9881c_screen_power_up(int dsi_id);
 extern int tinker_mcu_ili9881c_screen_power_off(int dsi_id);
 extern int tinker_mcu_ili9881c_is_connected(int dsi_id);
-//extern void tinker_ft5406_start_polling(int dsi_id);
+extern void tinker_ft5406_start_polling(int dsi_id);
 
 extern int lcd_size_flag[2];
 #endif
@@ -648,7 +648,7 @@ static int panel_simple_prepare(struct drm_panel *panel)
 	if (tinker_mcu_ili9881c_is_connected(p->dsi_id)) {
 		printk("tinker_mcu_ili9881c_screen_power_up\n");
 		tinker_mcu_ili9881c_screen_power_up(p->dsi_id);
-		//tinker_ft5406_start_polling(p->dsi_id);
+		tinker_ft5406_start_polling(p->dsi_id);
 	}
 	#endif
 
@@ -715,7 +715,7 @@ static int panel_simple_enable(struct drm_panel *panel)
 		printk("tinker_mcu_screen_power_up\n");
 		tinker_mcu_screen_power_up(p->dsi_id);
 		panel_simple_sleep(20);
-		//tinker_ft5406_start_polling(p->dsi_id);
+		tinker_ft5406_start_polling(p->dsi_id);
 	}
 
 	if (p->desc->init_seq) {
