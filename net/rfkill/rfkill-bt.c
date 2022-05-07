27923699fe7042eebdc2a0401a939d545fcfcce1
Xiao Yao <xiaoyao@rock-chips.com>
2022-05-07 16:29:39 +0800

    net: rfkill: bluetooth: workaround bt on/off issue for Debian
    Signed-off-by: Xiao Yao <xiaoyao@rock-chips.com>
    Change-Id: I1c637ada5c2bc54c33187cc29b0e39d158543878

--- net/rfkill/rfkill-bt.c
+++ net/rfkill/rfkill-bt.c
@@ -345,7 +345,7 @@ static int rfkill_rk_set_power(void *data, bool blocked)
 		if (gpio_is_valid(poweron->io)) {
 			if (gpio_get_value(poweron->io) == poweron->enable) {
 				gpio_direction_output(poweron->io,
-						      !poweron->enable);
+						      poweron->enable);
 				msleep(20);
 			}
 		}
@@ -355,7 +355,7 @@ static int rfkill_rk_set_power(void *data, bool blocked)
 		if (gpio_is_valid(reset->io)) {
 			if (gpio_get_value(reset->io) == reset->enable) {
 				gpio_direction_output(reset->io,
-						      !reset->enable);
+						      reset->enable);
 				msleep(20);
 			}
 		}
